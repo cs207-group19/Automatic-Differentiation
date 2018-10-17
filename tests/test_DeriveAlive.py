b0b401092e0808ff71c7ac9a75925dd321ec24dc
yue-stephanie <30400317+yue-stephanie@users.noreply.github.com>
2018-10-17 18:46:37 -0400

    Fixed typo of imported module name

--- tests/test_DeriveAlive.py
+++ tests/test_DeriveAlive.py
@@ -4,24 +4,24 @@ import numpy as np
 import math
 
 def test_DeriveAive_results():
-    x1 = ad.Var(np.pi)
+    x1 = da.Var(np.pi)
     f1 = 3 * x1.sin() +  3
     assert np.round(f1.val,2) == 3.0
     assert f1.der == -3.0
 
-    x4 = ad.Var(np.pi/4)
+    x4 = da.Var(np.pi/4)
     f4 = 3 * 2 * x4.tan() +  3
     assert f4.val == 9.0
     assert np.round(f4.der,2) == 12.0
 
-    x5= ad.Var(4.0)
+    x5= da.Var(4.0)
     f5 = x5.pow(3)
     assert f5.val == 64.0
     assert f5.der == 48.0
 
 def test_DeriveAlive_ZeroDivisionError():
     with pytest.raises(ZeroDivisionError):
-        x6 = ad.Var(0)
+        x6 = da.Var(0)
         f6 = x6.pow(1/2)
         assert f6.val == float('nan')
         assert f6.der == float('nan')
