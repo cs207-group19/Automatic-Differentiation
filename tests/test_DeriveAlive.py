3dac2a1b3c117205cd2c9a13f4c86c7efcbbe0cd
stephenslater <stephen.slater95@gmail.com>
2018-11-07 18:13:14 -0500

    Added tests for radd, rmul

--- tests/test_DeriveAlive.py
+++ tests/test_DeriveAlive.py
@@ -96,31 +96,41 @@ def test_DeriveAlive_scalar_functions():
 		assert x == y
 		assert x != z
 
+	def test_radd():
+		x = da.Var(5.0)
+		f = 2 + x
+		assert f == Var(7.0, 1.0)
+
+	def test_rmul():
+		x = da.Var(5.0)
+		f = 2 * x
+		assert f == Var(10, 2)
+
 	def test_div():
 		# Expect value of 1.5, derivative of 0.5
-		x3 = da.Var(3.0)
-		f3 = x3 / 2
-		assert np.round(f3.val, 2) == [1.5]
-		assert np.round(f3.der, 2) == [0.5]
+		x = da.Var(3.0)
+		f = x / 2
+		assert np.round(f.val, 2) == [1.5]
+		assert np.round(f.der, 2) == [0.5]
 
 	def test_rdiv():
 		# Expect value of 0.5, derivative of -0.25
-		x2 = da.Var(2.0)
-		f2 = 1 / x2
-		assert np.round(f2.val, 2) == [0.5]
-		assert np.round(f2.der, 2) == [-0.25]
+		x = da.Var(2.0)
+		f = 1 / x
+		assert np.round(f.val, 2) == [0.5]
+		assert np.round(f.der, 2) == [-0.25]
 
 	def test_sin():
 		# Expect value of 18.42, derivative of 6.0
-		x1 = da.Var(np.pi / 2)
-		f1 = 3 * 2 * x1.sin() + 2 * x1 + 4 * x1 + 3
-		assert np.round(f1.val, 2) == [18.42]
-		assert np.round(f1.der, 2) == [6.0]
+		x = da.Var(np.pi / 2)
+		f = 3 * 2 * x.sin() + 2 * x + 4 * x + 3
+		assert np.round(f.val, 2) == [18.42]
+		assert np.round(f.der, 2) == [6.0]
 
-		x11 = da.Var(np.pi)
-		f11 = 3 * x11.sin() +  3
-		assert np.round(f11.val, 2) == [3.0]
-		assert f11.der == [-3.0]
+		x2 = da.Var(np.pi)
+		f2 = 3 * x2.sin() +  3
+		assert np.round(f2.val, 2) == [3.0]
+		assert f2.der == [-3.0]
 
 	def test_cos():
 		# Expect value of -10pi, derivative of 0.0 (because of -sin(pi))
@@ -131,20 +141,20 @@ def test_DeriveAlive_scalar_functions():
 
 	def test_tan():
 		# Expect value of 9.0, derivative of 12.0
-		x4 = da.Var(np.pi / 4)
-		f4 = 3 * 2 * x4.tan() + 3
-		assert np.round(f4.val, 2) == [9.0]
-		assert np.round(f4.der, 2) == [12.0]
+		x = da.Var(np.pi / 4)
+		f = 3 * 2 * x.tan() + 3
+		assert np.round(f.val, 2) == [9.0]
+		assert np.round(f.der, 2) == [12.0]
 
-		x12 = da.Var(np.pi / 4)
-		f12 = 3 * 2 * x12.tan() + 3
-		assert f12.val == 9.0
-		assert np.round(f12.der, 2) == [12.0]
+		x2 = da.Var(np.pi / 4)
+		f2 = 3 * 2 * x2.tan() + 3
+		assert f2.val == 9.0
+		assert np.round(f2.der, 2) == [12.0]
 
 		# Tangent is undefined for multiples of pi/2 >= pi/2
 		with np.testing.assert_raises(ValueError):
-			x = da.Var(3 * np.pi / 2)
-			f = x.tan()
+			x3 = da.Var(3 * np.pi / 2)
+			f3 = x3.tan()
 
 	def test_arcsin():
 		x = da.Var(0)
@@ -194,29 +204,29 @@ def test_DeriveAlive_scalar_functions():
 
 	def test_pow():
 		# Expect value of 64.0, derivative of 48.0
-		x5 = da.Var(4.0)
-		f5 = x5.pow(3)
-		assert np.round(f5.val, 2) == [64.0]
-		assert np.round(f5.der, 2) == [48.0]
+		x = da.Var(4.0)
+		f = x.pow(3)
+		assert np.round(f.val, 2) == [64.0]
+		assert np.round(f.der, 2) == [48.0]
 	
 		# Divides by zero when computing derivative (0^{-1/2} term)
 		with np.testing.assert_raises(ZeroDivisionError):
 			zero = da.Var(0)
 			f_zero = zero.pow(1 / 2)
 
-		x6 = da.Var(4.0)
-		f6 = x6.pow(1 / 2)
-		assert np.round(f6.val, 2) == [2.0]
-		assert np.round(f6.der, 2) == [0.25]
+		x2 = da.Var(4.0)
+		f2 = x2.pow(1 / 2)
+		assert np.round(f2.val, 2) == [2.0]
+		assert np.round(f2.der, 2) == [0.25]
 
 		with np.testing.assert_raises(ValueError):
-			x7 = da.Var(-2)
-			f7 = x7.pow(1 / 2)
+			x3 = da.Var(-2)
+			f3 = x3.pow(1 / 2)
 
-		x13 = da.Var(4.0)
-		f13 = x13.pow(3)
-		assert f13.val == [64.0]
-		assert f13.der == [48.0]
+		x4 = da.Var(4.0)
+		f4 = x4.pow(3)
+		assert f4.val == [64.0]
+		assert f4.der == [48.0]
 
 	# TODO: Implement rpow in future implementation (not required for Milestone 2)
 	def test_rpow():
@@ -224,21 +234,21 @@ def test_DeriveAlive_scalar_functions():
 
 	def test_log():
 		# Expect value of 1.0, derivative of 0.23025850929940458
-		x8 = da.Var(10)
-		f8 = x8.log(10)
-		assert np.round(f8.val, 2) == [1.0]
-		assert np.round(f8.der, 2) == [0.23]
+		x = da.Var(10)
+		f = x.log(10)
+		assert np.round(f.val, 2) == [1.0]
+		assert np.round(f.der, 2) == [0.23]
 
 		with np.testing.assert_raises(ValueError):
-			x9 = da.Var(0)
-			f9 = x9.log(2)
+			x2 = da.Var(0)
+			f2 = x2.log(2)
 
 	def test_exp():
 		# Expect value of 2.718281828459045, derivative of 2.718281828459045
-		x10 = da.Var(1)
-		f10 = x10.exp()
-		assert np.round(f10.val, 2) == [2.72]
-		assert np.round(f10.der, 2) == [2.72]
+		x = da.Var(1)
+		f = x.exp()
+		assert np.round(f.val, 2) == [2.72]
+		assert np.round(f.der, 2) == [2.72]
 
 
 	# Run tests
