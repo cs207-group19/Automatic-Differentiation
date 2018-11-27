fixed abs() derivative

DeriveAlive/DeriveAlive.py
@@ -224,8 +224,12 @@ class Var(object):
 		if 0 in self.val:
 			raise ValueError("Absolute value is not differentiable at 0.")
 		#der = np.array([-1 if x < 0 else 1 for x in self.val])
-		der = abs(self.der)
-		return Var(val, der)
+		#der = abs(self.der)
+		der_copy = np.copy(self.der)
+		for i, val_i in enumerate(self.val):
+			if val_i < 0:
+				der_copy[i] = -1 * der_copy[i]
+		return Var(val, der_copy)
 
 	def __eq__(self, other):
 		try: