d44ed57340441995e4f78971447664717dea9dbb
yue-stephanie <30400317+yue-stephanie@users.noreply.github.com>
2018-12-10 09:31:11 -0500

    Added autodoc in Sphnix

--- documentation/docs/conf.py
+++ documentation/docs/conf.py
@@ -17,10 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
+import os
+import sys
 #sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../DeriveAlive'))
 
 
 # -- General configuration ------------------------------------------------
@@ -37,6 +37,10 @@ extensions = [
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.coverage',
+              'sphinx.ext.napoleon',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,6 +60,8 @@ project = 'DeriveAlive'
 copyright = '2018, Chen Shi, Stephen Slater, Yue Sun'
 author = 'Chen Shi, Stephen Slater, Yue Sun'
 
+import DeriveAlive
+
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
@@ -70,7 +76,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'Python'
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
