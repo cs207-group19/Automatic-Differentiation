0e841f761b291a53c669387a2e4aa5d62ae8ab2d
Chen Shi <cshi@hsph.harvard.edu>
2018-12-11 11:45:53 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
                  name="DeriveAlive",
-                 version="0.0.6",
+                 version="0.0.7",
                  author="Chen Shi, Stephen Slater, Yue Sun",
                  author_email="cshi@hsph.harvard.edu, stephenslater@college.harvard.edu, yuesun@g.harvard.edu",
                  description="An AutoDifferentiation package",
-                 long_description="This software library  will use the concept of automatic differentiation to solve differentiation problems in scientific computing.",
+                 long_description="The software library uses the concept of automatic differentiation to solve differentiation problems in scientific computing. Additional features of an optimization suite and a quadratic spline suite are also listed in this documentation.",
                  long_description_content_type="text/markdown",
                  url="https://github.com/cs207-group19/cs207-FinalProject",
                  packages=setuptools.find_packages(),
